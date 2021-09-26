bf5e5cae6ff5bdd383d8229966de943fa5fee543
Josh9722 <wellsy9722@gmail.com>
2021-09-26 19:51:41 +1000

    new js file

---
+++ js/jsgame.js
@@ -0,0 +1,20 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+    function stext() {
+        var x = document.getElementById("div1");
+        x.style.backgroundColor = red;
+    }
+
+    function htext() {
+        var x = document.getElementById("div1");
+        x.style.backgroundColor = '#6382CB';
+    }
+
+
+
